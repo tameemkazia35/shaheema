e9604823b13dc3401397a3880e4d7fe7ef31c35a
tameemkazia35 <tameemkazia35@gmail.com>
2019-05-01 20:42:29 +0400

    heart animation

--- js/index.js
+++ js/index.js
@@ -25,12 +25,14 @@ var redraw = function() {
   nextParticle.start();
 };
 
+
+
 var love = setInterval(function() {
-    var r_num = Math.floor(Math.random() * 20) + 1;
-    var r_size = Math.floor(Math.random() * 35) + 10;
-    var r_left = Math.floor(Math.random() * 50) + 1;
-    var r_bg = Math.floor(Math.random() * 15) + 100;
-    var r_time = Math.floor(Math.random() * 10) + 5;
+    var r_num = Math.floor(Math.random() * 40) + 1;
+    var r_size = Math.floor(Math.random() * 20) + 10;
+    var r_left = Math.floor(Math.random() * 100) + 1;
+    var r_bg = Math.floor(Math.random() * 25) + 100;
+    var r_time = Math.floor(Math.random() * 5) + 5;
 
     $('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
 
